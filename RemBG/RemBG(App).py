d17045efb181feffbd6879d3ff2f7d3e017b2e9e
wilsondesouza <wilson.de.souza.junior@gmail.com>
2023-08-19 21:58:09 -0300

    Reformulando pastas

--- RemBG/RemBG(App).py
+++ RemBG/RemBG(App).py
@@ -1,15 +1,29 @@
 # Programa simples, com GUI, para retirar o fundo de imagens com boas resoluções #
 
 from tkinter import *
+from tkinter import ttk
 from rembg import remove
 from PIL import Image, ImageTk
 from tkinter import filedialog
 
-root = Tk()
-root.title ('RemBG')
-root.geometry ('800x600')
+# Configurações da Interface #
+gui = Tk()
+gui.title ('RemBG')
+gui.geometry ('400x150')
+mainFrame = Frame(gui)
+mainFrame.pack(fill=BOTH, expand=1)
+myCanvas = Canvas(mainFrame)
+myCanvas.pack(side=LEFT, fill=BOTH, expand=1)
+scroll = ttk.Scrollbar(mainFrame, orient=VERTICAL, command=myCanvas.yview)
+scroll.pack(side=RIGHT, fill=Y)
+myCanvas.configure(yscrollcommand=scroll.set)
+myCanvas.bind('<Configure>', lambda e: myCanvas.configure(scrollregion=myCanvas.bbox("all")))
+segundoFrame = Frame(myCanvas)
+myCanvas.create_window((0,0), window=segundoFrame, anchor="nw")
 
-# Funções 
+# Configurações da Interface #
+
+# Funções 'Backend' #
 def open_thing():
     global input_path, my_img
     input_path = filedialog.askopenfilename(title='Selecionar Imagem', filetype=(("JPEG", ".jpeg"), ("Todos os tipos", "*.*")))
@@ -25,17 +39,17 @@ def remove_thing():
     global my_img
     my_img = ImageTk.PhotoImage(Image.open(output_path))
     pic_label.config(image=my_img)
+# Funções 'Backend' #
 
+# Interface Gráfica #
 
-# Janela
+pic_label = Label (segundoFrame, text='')
+pic_label.pack(pady=20, padx=200)
+open_button = Button(segundoFrame, text="Abrir Imagem", command=open_thing)
+open_button.pack(pady=10, padx=50)
 
-pic_label = Label (root, text='')
-pic_label.pack(pady=20)
+remove_button = Button(segundoFrame, text='Remover Background', command=remove_thing)
+remove_button.pack(pady=5, padx=50)
+# Interface Gráfica #
 
-open_button = Button(root, text="Abrir Imagem", command=open_thing)
-open_button.pack(pady=20)
-
-remove_button = Button(root, text='Remover Background', command=remove_thing)
-remove_button.pack(pady=20)
-
-root.mainloop()
+gui.mainloop()
---
+++ RewardMicrosoft/Readme.txt
@@ -0,0 +1,27 @@
+Leia-me até o final
+
+- GamePass.exe (versão codeline e com interface gráfica) -
+Para que o programa funcione sem maiores problemas e seja executado até o final, além das observações feitas
+dentro do próprio programa, onde é pedido a instalação do emulador "Bluestacks" e do game "Fallout Shelter",
+é necessário executá-lo sem outros programas, pastas ou navegadores abertos, onde seus ícones ocupam espaço 
+na barra de tarefas. Isto é necessário, pois parte do código do programa utiliza da biblioteca PyAutoGui, 
+onde se faz necessário a captura do clique do mouse em posições específicas da tela.
+
+- PesquisaBing.exe (versão codeline e com interface gráfica) -
+Sem maiores etapas e observações para executar além das apontadas dentro do próprio programa. 
+O programa utiliza do selenium para automatizar os comandos
+(por favor, deixe o Edge como navegador padrão antes e enquanto o programa é executado).
+
+Read-me untilthe end
+
+- GamePass.exe (Codeline version and GUI version)
+For the program to run smoothly and run until the end, in addition to the observations made
+within the program itself, where the installation of the "Bluestacks" emulator and the game "Fallout Shelter" 
+is requested, it is necessary to run it without other programs, folders or browsers open, where its icons take up space
+spaces on the taskbar. This is necessary, because part of the program code uses the PyAutoGui library, where 
+it is necessary to capture mouse clicks in specific positions on the screen.
+
+- PesquisaBing.exe (Codeline version and GUI version)
+No further steps and observations to perform other than those pointed out within the program itself.
+The program uses selenium to automate the commands
+(please put Edge as default browser before and while running the program).
